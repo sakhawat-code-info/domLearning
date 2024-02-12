977f36e1d24682b542271a44cd3f409babdf16c0
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-12 16:29:59 +0600

    tazbi code added

---
+++ script/practice.js
@@ -0,0 +1,92 @@
+
+
+
+
+/* ****************************** tazbi code****************************************** */
+
+// const subahanAllahDisplay = document.getElementById('subahanAllahDisplay');
+// const subahanAllahIncrimentBtn = document.getElementById('subahanAllahincrimentBtn');
+// const subahanAllahDecrimentBtn = document.getElementById('subahanAllahdecrimentBtn');
+
+// let subahanAllahCount = 0;
+
+// subahanAllahIncrimentBtn.addEventListener('click', function () {
+//     if (subahanAllahCount === 33) {
+//         return alert("Completed");
+//     }
+//     subahanAllahCount += 1;
+//     subahanAllahDisplay.innerText = subahanAllahCount;
+// })
+
+// subahanAllahDecrimentBtn.addEventListener('click', function () {
+//     if (subahanAllahCount === 0) {
+//         return alert("can't add negative value");
+//     }
+
+//     subahanAllahCount -= 1;
+//     subahanAllahDisplay.innerText = subahanAllahCount;
+// })
+
+// let alhamduAllahCount = 0;
+
+// const alhamduAllahDisplay = document.getElementById('alhamduAllahDisplay');
+// const alhamduAllahIncrimentBtn = document.getElementById('alhamduAllahIncrimentBtn');
+// const alhamduAllahDecrimentBtn = document.getElementById('alhamduAllahDecrimentBtn');
+
+// alhamduAllahIncrimentBtn.addEventListener('click', function () {
+//     if (alhamduAllahCount === 33) {
+//         return alert("AlhamduAllah Completed next plz")
+//     }
+//     alhamduAllahCount += 1;
+//     alhamduAllahDisplay.innerText = alhamduAllahCount;
+// })
+
+// alhamduAllahDecrimentBtn.addEventListener('click', function () {
+//     if (alhamduAllahCount === 0) {
+//         return alert('Can not added negative value');
+//     }
+//     alhamduAllahCount -= 1;
+//     alhamduAllahDisplay.innerText = alhamduAllahCount;
+// })
+
+// let allahAkbarCount = 0;
+
+// const allahAkbarDisplay = document.getElementById('allahAkbarDisplay');
+// const allahAkbarIncrimentBtn = document.getElementById('allahAkbarIncrimentBtn');
+// const allahAkbarDecrimentBtn = document.getElementById('allahAkbarDecrimentBtn');
+
+
+// allahAkbarIncrimentBtn.addEventListener('click', function () {
+//     if (allahAkbarCount === 34) {
+//         return alert('All Completed')
+//     }
+//     allahAkbarCount += 1;
+//     allahAkbarDisplay.innerText = allahAkbarCount;
+// })
+// allahAkbarDecrimentBtn.addEventListener('click', function () {
+//     if (allahAkbarCount === 0) {
+//         return alert('can not minus')
+//     }
+//     allahAkbarCount -= 1;
+//     allahAkbarDisplay.innerText = allahAkbarCount;
+// })
+
+
+// document.getElementById('resetBtn').addEventListener('click', function () {
+//     allahAkbarDisplay.innerText = 0;
+//     allahAkbarCount = 0
+
+//     alhamduAllahCount = 0;
+//     alhamduAllahDisplay.innerText = 0;
+
+//     subahanAllahDisplay.innerText = 0;
+//     subahanAllahCount = 0;
+// })
+
+
+
+
+
+
+
+
